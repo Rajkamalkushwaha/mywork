6dff79eab47aa114e604ae4a5a7bb0e520d83584
Rajkamalkushwaha <36695253+Rajkamalkushwaha@users.noreply.github.com>
2018-03-12 12:41:58 +0530

    complete

--- Asynchronous_promise.js
+++ Asynchronous_promise.js
@@ -1,5 +1,30 @@
 //Asynchronous promise
 
+var isMomHappy=true;
+
+//Create Promises
+var willIGetNewPhone=new Promise(function (resolve,reset){
+     if(isMomHappy){
+	var phone={
+       color:'black',
+	   band:'samsung'       
+       };
+	resolve(phone);
+  }else{
+   var erro=new Error("Mom is not happy");
+     reset(erro);
+  }
+});
+//make 2nd promises
+var showFriend=function(phone){
+	return new Promise(function(resolve,reject){     //fulfilled 
+     var message='Hey friend, I have a new' + phone.color+' '+phone.brand+'phone';
+		
+     resolve(message);
+     }
+   )
+};
+//call promise 
 var askMom=function(){
       console.log('before ask mom');
 	  willIGetNewPhone
@@ -13,6 +38,10 @@ var askMom=function(){
 	   });
    console.log('After ask Mom');
 };
+
+askMom();
+
+
 
 
 //output
